006ed31304f8f80453129e0d7a327ce644b6d38c
Fariba Shamseh <fariba.shamseh0@gmail.com>
2024-10-23 17:49:53 +0330

    Added reusability to StarRating by passing props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import StarRating from "./StarRating.jsx";
 
 export default function App(){
     return(
-        <StarRating maxRating={10} />
+        <>
+            <StarRating maxRating={5} messages={["Terrible","Bad","Okay","Good","Amazing"]} defaultRating={3} />
+            {/*<StarRating size={24} color="red" className="test" />*/}
+        </>
     )
 }
